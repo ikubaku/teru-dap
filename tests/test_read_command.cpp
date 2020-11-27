a14f419baf2e26a07438c882963ac27105cb9b66
ikubaku <hide4d51@gmail.com>
2020-11-28 07:54:32 +0900

    Update the test

--- tests/test_read_command.cpp
+++ tests/test_read_command.cpp
@@ -1,8 +1,17 @@
 #include "gtest/gtest.h"
 #include "../teru_dap.h"
 
-TEST(ReadCommandTest, ReadDAP_CMD_INFO) {
-    const uint8_t bytes[] = {0x00, 0x01};    // Request the vendor id
-    EXPECT_EQ(read_command(bytes), TERU_ACT_INFO);
-    EXPECT_EQ(dap_info, DAP_INFO_VENDOR_ID);
+class CommandReaderTest : public ::testing::Test {
+protected:
+    CommandReader reader;
+};
+
+TEST_F(CommandReaderTest, ReadDAP_CMD_INFO) {
+    uint8_t bytes[] = {0x00, 0x01};    // Request the vendor id
+
+    uint8_t act = reader.read_command(bytes);
+    ASSERT_EQ(act, TERU_ACT_INFO);
+
+    uint8_t info_id = reader.get_requested_dap_info_id();
+    ASSERT_EQ(info_id, DAP_INFO_VENDOR_ID);
 }
